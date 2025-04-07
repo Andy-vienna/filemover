fc7c57c4fef827417b97d27dd62c9de3c980fa17
Andy-vienna <andreas.fischer@aw-net.eu>
2025-04-07 10:06:08 +0200

    0.1.1-alpha.1

--- src/org/andy/filemover/FileMover.java
+++ src/org/andy/filemover/FileMover.java
@@ -1,15 +1,18 @@
 package org.andy.filemover;
 
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.channels.FileChannel;
+import java.io.RandomAccessFile;
+import java.net.URISyntaxException;
+import java.nio.channels.FileLock;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardOpenOption;
-import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
@@ -26,64 +29,113 @@ import java.util.stream.Collectors;
 
 public class FileMover {
 
-	private final Logger logger = Logger.getLogger(FileMover.class.getName());
+	private static final Logger logger = Logger.getLogger(FileMover.class.getName());
 
-	private final Properties config;
-	private final Set<String> fileExtensions;
-	private final Path sourceDir;
-	private final Path targetDir;
+	private static Properties config = new Properties();
+	private static Path folderToWatch = null;
+	private static Path targetDir = null;
+	private static String[] fileExtensions = null;
+	private static Set<String> artifactExtensions;
 
 	// ###################################################################################################################################################
 	// ###################################################################################################################################################
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
+
+		try {
+			setupLogger();
+			config = loadConfig();
+		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		folderToWatch = Paths.get(config.getProperty("watch.path"));
+		targetDir = Paths.get(config.getProperty("target.path"));
+		fileExtensions = config.getProperty("watch.extension").split(",");
+
 		try {
 			new FileMover().startWatching();
+			logger.info("FileMover überwacht: " + folderToWatch);
 		} catch (IOException | InterruptedException e) {
-			e.printStackTrace();
+			logger.severe("Fehler bei Start der Überwachung: " + e.getMessage());
 		}
 	}
 
+	/**
+	 * @throws IOException
+	 */
 	public FileMover() throws IOException {
-		setupLogger();
-		config = loadConfig();
-		fileExtensions = loadExtensions("watch.extension");
-		sourceDir = Paths.get(config.getProperty("watch.path"));
-		targetDir = Paths.get(config.getProperty("target.path"));
+		// Setze artifactExtensions im Konstruktor
+		FileMover.artifactExtensions = loadArtifactExtensions();
 	}
 
 	// ###################################################################################################################################################
 	// ###################################################################################################################################################
 
+	/**
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	private void startWatching() throws IOException, InterruptedException {
+
 		WatchService watchService = FileSystems.getDefault().newWatchService();
-		sourceDir.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
-		logger.info("Überwache Verzeichnis: " + sourceDir);
+		folderToWatch.register(watchService, ENTRY_CREATE);
 
 		while (true) {
 			WatchKey key = watchService.take();
 
 			for (WatchEvent<?> event : key.pollEvents()) {
-				if (event.kind() == StandardWatchEventKinds.OVERFLOW) {
+				WatchEvent.Kind<?> kind = event.kind();
+				if (kind == OVERFLOW) {
 					continue;
 				}
 
 				@SuppressWarnings("unchecked")
 				WatchEvent<Path> ev = (WatchEvent<Path>) event;
+
 				Path fileName = ev.context();
 				String fileNameLower = fileName.toString().toLowerCase();
 
-				if (fileExtensions.stream().anyMatch(fileNameLower::endsWith)) {
-					Path sourcePath = sourceDir.resolve(fileName);
+				boolean matchesExtension = Arrays.stream(fileExtensions).map(String::toLowerCase) // Konvertiere auch
+						// die Endungen in
+						// Kleinbuchstaben
+						.anyMatch(fileNameLower::endsWith);
+
+				if (matchesExtension) {
+					Path sourcePath = folderToWatch.resolve(fileName);
 					Path targetPath = targetDir.resolve(fileName);
 
-					while (isLocked(sourcePath)) {
-						Thread.sleep(500);
+					boolean bWaitSmo = isLocked(fileNameLower);
+					while (bWaitSmo) {
+						Thread.sleep(1000);
+						bWaitSmo = isLocked(fileNameLower);
 					}
 
 					try {
 						Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
 						logger.info("Datei verschoben: " + fileName);
+
+						// Überprüfe und lösche Artefakte im aktuellen Verzeichnis
+						File dir = new File(System.getProperty("user.dir"));
+						for (File f : dir.listFiles()) {
+							if (f.isFile()) {
+								// System.out.println("Gefundene Datei: " + f.getName() + ", Größe: " +
+								// f.length());
+								if (f.length() == 0 && isArtifact(f.getName())) {
+									System.out.println("Artefakt gefunden: " + f.getName());
+									boolean deleted = f.delete(); // Versuche, das Artefakt zu löschen
+									if (deleted) {
+										System.out.println("Artefakt gelöscht: " + f.getName());
+									} else {
+										System.out.println("Fehler beim Löschen von " + f.getName());
+									}
+								}
+							}
+						}
+
 					} catch (IOException e) {
 						logger.severe("Fehler beim Verschieben: " + e.getMessage());
 					}
@@ -91,51 +143,19 @@ public class FileMover {
 			}
 
 			if (!key.reset()) {
-				logger.info("Beobachtung beendet.");
+				logger.info("FileMover beendet.");
 				break;
 			}
 		}
 	}
 
-	private boolean isLocked(Path path) {
-		try (FileChannel channel = FileChannel.open(path, StandardOpenOption.WRITE)) {
-			channel.size();
-			return false;
-		} catch (IOException e) {
-			return true;
-		}
-	}
+	// ###################################################################################################################################################
+	// ###################################################################################################################################################
 
-	private Set<String> loadExtensions(String key) {
-		String extStr = config.getProperty(key);
-		if (extStr == null || extStr.isEmpty()) {
-			return Set.of();
-		}
-
-		return Arrays.stream(extStr.split(","))
-				.map(String::trim)
-				.map(String::toLowerCase)
-				.collect(Collectors.toSet());
-	}
-
-	private Properties loadConfig() throws IOException {
-		Properties props = new Properties();
-
-		String jarPath = FileMover.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-		if (jarPath.startsWith("/")) {
-			jarPath = jarPath.substring(1);
-		}
-		Path jarDir = Paths.get(jarPath).getParent();
-		Path configPath = jarDir.resolve("config.properties");
-
-		try (InputStream in = Files.newInputStream(configPath)) {
-			props.load(in);
-		}
-
-		return props;
-	}
-
-	private void setupLogger() throws IOException {
+	/**
+	 * @throws IOException
+	 */
+	private static void setupLogger() throws IOException {
 		LogManager.getLogManager().reset();
 		Logger rootLogger = Logger.getLogger("");
 		ConsoleHandler consoleHandler = new ConsoleHandler();
@@ -147,5 +167,93 @@ public class FileMover {
 		fileHandler.setFormatter(new SimpleFormatter());
 		rootLogger.addHandler(fileHandler);
 	}
+
+	/**
+	 * @return
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	private static Properties loadConfig() throws IOException, URISyntaxException {
+		var props = new Properties();
+
+		// Ermittel die URI des JARs, das die FileWatcher-Klasse enthält.
+		var jarUri = FileMover.class.getProtectionDomain().getCodeSource().getLocation().toURI();
+
+		// Konvertiere die URI in einen Path – so werden Encodingprobleme (z. B.
+		// führende Slash) automatisch behoben.
+		var jarPath = Paths.get(jarUri);
+		var jarDir = jarPath.getParent();
+
+		var configPath = jarDir.resolve("config.properties");
+
+		try (var in = Files.newInputStream(configPath)) {
+			props.load(in);
+		}
+
+		return props;
+	}
+
+	/**
+	 * @param fileName
+	 * @return
+	 */
+	private static boolean isLocked(String fileName) {
+		try (RandomAccessFile randomAccessFile = new RandomAccessFile(fileName, "rw");
+				FileLock lock = randomAccessFile.getChannel().lock()) {
+			return lock == null;
+		} catch (IOException ex) {
+			return true;
+		}
+	}
+
+	/**
+	 * @return
+	 * @throws IOException
+	 */
+	private static Set<String> loadArtifactExtensions() throws IOException {
+		String extensions = config.getProperty("watch.extension");
+
+		// Debug-Ausgabe der geladenen Konfiguration
+		// System.out.println("Geladene Konfiguration (watch.extension): " +
+		// extensions);
+
+		if (extensions == null || extensions.isEmpty()) {
+			return Set.of(); // Falls keine Endungen angegeben sind, leer zurückgeben
+		}
+
+		// Entferne führende/abschließende Leerzeichen und stelle sicher, dass jede
+		// Endung mit einem Punkt beginnt
+		return Arrays.stream(extensions.split(",")).map(String::trim) // Entferne Leerzeichen
+				.map(e -> e.startsWith(".") ? e : "." + e) // Stelle sicher, dass jede Endung mit einem Punkt beginnt
+				.collect(Collectors.toSet());
+	}
+
+	/**
+	 * @param fileName
+	 * @return
+	 */
+	private boolean isArtifact(String fileName) {
+		// Konvertiere den Dateinamen und die Endungen in Kleinbuchstaben
+		String lowerCaseFileName = fileName.toLowerCase();
+
+		// Debug-Ausgabe, um zu sehen, welche Endungen überprüft werden
+		// System.out.println("Überprüfe Datei: " + fileName);
+
+		for (String extension : artifactExtensions) {
+			// Konvertiere die Endung ebenfalls in Kleinbuchstaben, um die Vergleichbarkeit
+			// zu gewährleisten
+			String lowerCaseExtension = extension.toLowerCase();
+
+			// Debug-Ausgabe der Endungen
+			// System.out.println("Vergleiche mit Endung: " + lowerCaseExtension);
+
+			if (lowerCaseFileName.endsWith(lowerCaseExtension)) {
+				// System.out.println("Passende Endung gefunden: " + lowerCaseExtension);
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
 
